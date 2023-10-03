38f6005cf69763c786e3c5108f442f861d826ab3
Anastasia <2804ad@gmail.com>
2023-10-03 23:25:44 +0300

    Second task

--- MainApp/views.py
+++ MainApp/views.py
@@ -8,3 +8,13 @@ def home(request):
     """
 
     return HttpResponse(text)
+
+def about(request):
+    text = """<p> Имя: <b>Анастасия</b><br>
+               Отчество: <b>Александровна</b><br>
+               Фамилия: <b>Дудченко</b><br>
+               телефон: <b>8-923-600-03-03</b><br>
+               email: <b>andud@mail.ru</b></p>
+    """
+
+    return HttpResponse(text)
